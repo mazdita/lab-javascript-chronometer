02d2ced0008f2e824475dc7543528c6aee597be8
mazdita <mazdita@hotmail.com>
2021-03-29 20:47:12 +0200

    revisar cronometro

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,21 +1,25 @@
 class Chronometer {
+ 
   constructor() {
-    // ... your code goes here
+    this.currentTime='0';
+    this.intervalId='0';
   }
   startClick(callback) {
-    // ... your code goes here
+    serInterval(()=>{
+      this.currentTime++;
+    },1000);
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60);
   }
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime%60);
   }
   twoDigitsNumber() {
     // ... your code goes here
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
     // ... your code goes here
